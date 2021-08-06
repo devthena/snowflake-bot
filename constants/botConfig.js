b9066666983abf53b3a224608538eeb43d6d0aee
AthenaUS <theathenaus@gmail.com>
2021-08-05 17:32:39 -0700

    version bump 1.8.0-alpha

--- constants/botConfig.js
+++ constants/botConfig.js
@@ -11,7 +11,7 @@ const config = {
     COMMANDS: 'https://snowflakebot.com/commands',
     FAQ: 'https://snowflakebot.com/faq'
   },
-  VERSION: '1.7.4'
+  VERSION: '1.8.0-alpha'
 };
 
 module.exports = config;
