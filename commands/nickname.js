fc81054a6106ab7564d25af879b3148782e141b6
AthenaUS <theathenaus@gmail.com>
2021-11-09 13:22:39 -0800

    fix(nickname): send progress messages in channel instead of replying to interaction

--- commands/nickname.js
+++ commands/nickname.js
@@ -4,14 +4,15 @@ module.exports = async interaction => {
 
   const format = interaction.options.getString('format');
   const role = interaction.options.getRole('role');
+  const channel = interaction.channel;
 
   const filteredMembers = interaction.guild.members.cache.filter(member => member.roles.cache.find(memberRole => memberRole.id === role.id));
-  const filtered = Array.from(filteredMembers.values());;
+  const filtered = Array.from(filteredMembers.values());
 
   if (filtered.length > 0) {
 
     try {
-      await interaction.reply(`Found: **${filtered.length}** | Updated: **0** | Processed: **0%**`);
+      await interaction.reply(`Found: **${filtered.length}** | Processing...`);
     } catch(err) { console.error(err); }
 
     let processedCount = 0;
@@ -21,7 +22,8 @@ module.exports = async interaction => {
     await asyncForEach(filtered, async member => {
 
       if(member.manageable) {
-        const nickname = format.replace('name', member.user.username);
+        const name = member.user.username.length > 28 ? member.user.username.slice(0,29) : member.user.username;
+        const nickname = format.replace('name', name);
         try {
           await member.setNickname(nickname);
           updatedCount += 1;
@@ -35,24 +37,24 @@ module.exports = async interaction => {
 
       if(percent === 100) {
 
-        let finalMessage = `Found: **${filtered.length}** | Updated: **${ updatedCount }** | Processed: **100%** :white_check_mark:`;
+        let finalMessage = `Total Updated: **${ updatedCount }** | Processed: **100%** :white_check_mark:`;
         if(processedCount !== updatedCount) finalMessage += `\n_(some members were not updated due to role hierarchy)_`;
-        await interaction.editReply(finalMessage);
+        await channel.send(finalMessage);
 
       } else if (percent >= 75 && progress !== 75) {
 
         progress = 75;
-        await interaction.editReply(`Found: **${filtered.length}** | Updated: **${ updatedCount }** | Processed: **75%**`);
+        await channel.send(`Updated: **${ updatedCount }** | Processed: **75%**`);
 
       } else if (percent >= 50 && percent < 75 && progress !== 50) {
 
         progress = 50;
-        await interaction.editReply(`Found: **${filtered.length}** | Updated: **${ updatedCount }** | Processed: **50%**`);
+        await channel.send(`Updated: **${ updatedCount }** | Processed: **50%**`);
 
       } else if(percent >= 25 && percent < 50 && progress !== 25) {
 
         progress = 25;
-        await interaction.editReply(`Found: **${filtered.length}** | Updated: **${ updatedCount }** | Processed: **25%**`);
+        await channel.send(`Updated: **${ updatedCount }** | Processed: **25%**`);
       }
 
     });
