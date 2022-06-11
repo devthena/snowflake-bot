913da86488ac130b3b655703b2a7b295313944f6
Nesceal Delizo <ndelizo@videoamp.com>
2022-06-10 20:05:08 -0700

    refactor(MessageEmbed): update setAuthor and setFooter to receive object parameters

--- events/presenceUpdate.js
+++ events/presenceUpdate.js
@@ -1,4 +1,3 @@
-
 const { MessageEmbed } = require('discord.js');
 const isTrue = require('../helpers/isTrue');
 const { COLOR } = require('../constants/botConfig');
@@ -8,27 +7,34 @@ const types = require('./../constants/activityTypes');
  * Adds a role to determine which members are currently live streaming
  * For server owners, announces the live stream when enabled
  * @listens event:presenceUpdate
- * @param {Client} Bot 
- * @param {Presence} oldPresence 
- * @param {Presence} newPresence 
+ * @param {Client} Bot
+ * @param {Presence} oldPresence
+ * @param {Presence} newPresence
  */
 module.exports = (Bot, oldPresence, newPresence) => {
-
   if (!newPresence.guild?.available) return;
 
   const server = Bot.servers.get(newPresence.guild.id);
   if (!server) return;
 
-  let liveRole = newPresence.guild.roles.cache.find(role => role.name.includes('Now Live'));
+  let liveRole = newPresence.guild.roles.cache.find(role =>
+    role.name.includes('Now Live')
+  );
   let newMember = newPresence.member;
 
   // member went offline
   if (newPresence.status === 'offline') {
-    if (liveRole && newMember.manageable && newMember.roles.cache.has(liveRole.id)) {
-      newMember.roles.remove(liveRole)
+    if (
+      liveRole &&
+      newMember.manageable &&
+      newMember.roles.cache.has(liveRole.id)
+    ) {
+      newMember.roles
+        .remove(liveRole)
         .then(function () {
           Bot.logger.info(`${newMember.displayName} went offline.`);
-        }).catch(function (error) {
+        })
+        .catch(function (error) {
           Bot.logger.error(`presenceUpdate: ${error}`);
         });
     }
@@ -37,73 +43,105 @@ module.exports = (Bot, oldPresence, newPresence) => {
 
   // member has activities
   if (newPresence.activities.length) {
-
     // member has started streaming
-    const isStreaming = newPresence.activities.some(activity => activity.type === types.streaming);
-    const hasBeenStreaming = oldPresence ? oldPresence.activities.some(activity => activity.type === types.streaming) : false;
+    const isStreaming = newPresence.activities.some(
+      activity => activity.type === types.streaming
+    );
+    const hasBeenStreaming = oldPresence
+      ? oldPresence.activities.some(
+          activity => activity.type === types.streaming
+        )
+      : false;
 
     if (!hasBeenStreaming && isStreaming) {
-
-      if (liveRole && newMember.manageable && !newMember.roles.cache.has(liveRole.id)) {
-        newMember.roles.add(liveRole)
+      if (
+        liveRole &&
+        newMember.manageable &&
+        !newMember.roles.cache.has(liveRole.id)
+      ) {
+        newMember.roles
+          .add(liveRole)
           .then(function () {
             Bot.logger.info(`${newMember.displayName} started streaming.`);
-          }).catch(function (error) {
+          })
+          .catch(function (error) {
             Bot.logger.error(`presenceUpdate: ${error}`);
           });
       }
 
       // stream announcements for server owners
-      if (isTrue(server.mods.alertStream) && newPresence.guild.ownerId === newMember.id) {
-
-        const streamActivity = newPresence.activities.find(activity => activity.type === types.streaming);
-        const alertStreamChannel = newPresence.guild.channels.cache.find(channel => channel.name.includes(server.channels.alertStream));
+      if (
+        isTrue(server.mods.alertStream) &&
+        newPresence.guild.ownerId === newMember.id
+      ) {
+        const streamActivity = newPresence.activities.find(
+          activity => activity.type === types.streaming
+        );
+        const alertStreamChannel = newPresence.guild.channels.cache.find(
+          channel => channel.name.includes(server.channels.alertStream)
+        );
 
         if (streamActivity && alertStreamChannel) {
-
           let liveMessage = '';
           if (streamActivity.details) liveMessage += streamActivity.details;
           if (streamActivity.url) liveMessage += `\n\n${streamActivity.url}`;
 
-          let liveImage = streamActivity.assets ? streamActivity.assets.largeImageURL() : null;
+          let liveImage = streamActivity.assets
+            ? streamActivity.assets.largeImageURL()
+            : null;
 
           const botEmbed = new MessageEmbed()
-            .setAuthor(newMember.user.username, newMember.user.displayAvatarURL())
+            .setAuthor({
+              name: newMember.user.username,
+              iconURL: newMember.user.displayAvatarURL(),
+            })
             .setColor(COLOR)
             .setTitle(`Now Streaming ${streamActivity.state}`)
             .setDescription(liveMessage)
             .setImage(liveImage)
-            .setFooter(`Posted on ${streamActivity.createdAt.toDateString()}`);
+            .setFooter({
+              text: `Posted on ${streamActivity.createdAt.toDateString()}`,
+            });
 
-          alertStreamChannel.send(`@everyone ${newMember.user.username} is now live!`)
-            .then(() => { alertStreamChannel.send({ embeds: [botEmbed] }); })
+          alertStreamChannel
+            .send(`@everyone ${newMember.user.username} is now live!`)
+            .then(() => {
+              alertStreamChannel.send({ embeds: [botEmbed] });
+            })
             .catch(error => Bot.logger.error(`presenceUpdate: ${error}`));
         }
       }
-
     } else if (hasBeenStreaming && !isStreaming) {
-
       // member stopped streaming
-      if (liveRole && newMember.manageable && newMember.roles.cache.has(liveRole.id)) {
-        newMember.roles.remove(liveRole)
+      if (
+        liveRole &&
+        newMember.manageable &&
+        newMember.roles.cache.has(liveRole.id)
+      ) {
+        newMember.roles
+          .remove(liveRole)
           .then(function () {
             Bot.logger.info(`${newMember.displayName} is done streaming.`);
-          }).catch(function (error) {
+          })
+          .catch(function (error) {
             Bot.logger.error(`presenceUpdate: ${error}`);
           });
       }
     }
-
   } else {
-
-    if (liveRole && newMember.manageable && newMember.roles.cache.has(liveRole.id)) {
-      newMember.roles.remove(liveRole)
+    if (
+      liveRole &&
+      newMember.manageable &&
+      newMember.roles.cache.has(liveRole.id)
+    ) {
+      newMember.roles
+        .remove(liveRole)
         .then(function () {
           Bot.logger.info(`${newMember.displayName} is done streaming.`);
-        }).catch(function (error) {
+        })
+        .catch(function (error) {
           Bot.logger.error(`presenceUpdate: ${error}`);
         });
     }
   }
-
 };
--- helpers/serverLog.js
+++ helpers/serverLog.js
@@ -6,27 +6,27 @@ const logServerId = process.env.LOG_SERVER_ID;
 /**
  * Log events in a Discord server
  * @param {Client} Bot
- * @param {String} logEvent 
+ * @param {String} logEvent
  */
 const log = (Bot, logEvent) => {
-
   const logServer = Bot.guilds.cache.get(logServerId);
 
   if (logServer) {
-
     const logChannel = logServer.channels.cache.find(channel => {
       return channel.name.includes(LOG_CHANNEL) && channel.isText();
     });
 
     if (logServer.available && logChannel) {
-
       let botEmbed = new MessageEmbed()
-        .setAuthor(`${logEvent.author} Server`, `${logEvent.authorIcon || ''}`)
+        .setAuthor({
+          name: `${logEvent.author} Server`,
+          iconURL: logEvent.authorIcon || '',
+        })
         .setDescription(logEvent.message)
         .setColor(statusColors[logEvent.type]);
-      
-      if(logEvent.thumbnail) botEmbed.setThumbnail(logEvent.thumbnail);
-      if(logEvent.footer) botEmbed.setFooter(logEvent.footer);
+
+      if (logEvent.thumbnail) botEmbed.setThumbnail(logEvent.thumbnail);
+      if (logEvent.footer) botEmbed.setFooter({ text: logEvent.footer });
 
       return logChannel.send({ embeds: [botEmbed] });
     }
